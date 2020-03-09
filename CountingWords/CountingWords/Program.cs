fb836bcd4f7e38c7394b4a619a9e9b1cfcf16dbe
GoonerJono <jsabate014@gmail.com>
2020-03-09 18:06:53 +0200

    new project

---
+++ CountingWords/CountingWords/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using GroupDocs.Parser;
+
+namespace CountingWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            using (Parser parser = new Parser(@"C:\Users\xboxl\OneDrive\Documents\2600-0.txt"))
+            {
+                using (TextReader reader = parser.GetText())
+                {
+                     text = reader.ReadToEnd();
+                }
+            }
+            // var test = "This is a new sentence used to determine how many words are in the sentence";
+            char[] chars = { ' ', '.', ',', ';', ':', '?', '\n', '\r' };
+            var newtest = text.Split(chars);
+            var testing = CountWords(newtest);
+            Console.WriteLine($"Total count: {testing.Count}");
+            foreach(var t in testing)
+            {
+                Console.WriteLine($"Total occurrences of {t.Key}: {t.Value}");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static Dictionary<string, int> CountWords(string[] words)
+        {
+            
+            Dictionary<string, int> wordandcount = new Dictionary<string, int>();
+            foreach(var word in words)
+            {
+                if(!wordandcount.ContainsKey(word))
+                {
+                    wordandcount.Add(word,1);
+                } else
+                {
+                    wordandcount[word] += 1;
+                }
+            }
+            return wordandcount;
+        }
+    }
+}
